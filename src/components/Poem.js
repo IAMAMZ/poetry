e4ebff7bca839697dbe1b127a50a8efe6a69681b
Miran Qarachatani <miranararat722@gmail.com>
2023-08-08 12:11:43 -0400

    add save btn to each poem

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,3 +1,4 @@
+import { baseUrl } from "../shared";
 import "./Poem.css";
 
 export default function Poem({ title, author, lines }) {
@@ -10,6 +11,34 @@ export default function Poem({ title, author, lines }) {
         {lines.map((line) => (
           <p>{line}</p>
         ))}
+        <button
+          onClick={() => {
+            const poemObj = JSON.stringify({
+              title: title,
+              author: author,
+              lines: lines,
+            });
+            fetch(baseUrl + "/api/poems", {
+              headers: {
+                "Content-Type": "application/json",
+              },
+              method: "POST",
+              body: poemObj,
+            })
+              .then((response) => {
+                if (!response.ok) {
+                  throw new Error("Network response was not ok");
+                }
+                return response.json();
+              })
+              .then((data) => {
+                console.log(data);
+              })
+              .catch();
+          }}
+        >
+          Save Poem
+        </button>
       </div>
     </div>
   );
