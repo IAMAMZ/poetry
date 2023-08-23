165981931ad34c47d217fce41b0e7598a36d4cad
Miran Qarachatani <miranararat722@gmail.com>
2023-08-23 11:43:04 -0400

    add auth provider for global auth state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,10 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+import axios from "../api/axios";
+const LOGIN_URL = "/login";
 
 export default function Login() {
+  const { setAuth } = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
 
@@ -13,42 +17,37 @@ export default function Login() {
     setErrMsg("");
   }, [user, pwd]);
 
-  useEffect(() => {
-    userRef.current.focus();
-  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setSuccess(true);
 
-    //     try {
-    //       const response = await axios.post(
-    //         LOGIN_URL,
-    //         JSON.stringify({ user, pwd }),
-    //         {
-    //           headers: { "Content-Type": "application/json" },
-    //           withCredentials: true,
-    //         }
-    //       );
-    //       console.log(JSON.stringify(response?.data));
-    //       //console.log(JSON.stringify(response));
-    //       const accessToken = response?.data?.accessToken;
-    //       const roles = response?.data?.roles;
-    //       setAuth({ user, pwd, roles, accessToken });
-    //       setUser("");
-    //       setPwd("");
-    //       setSuccess(true);
-    //     } catch (err) {
-    //       if (!err?.response) {
-    //         setErrMsg("No Server Response");
-    //       } else if (err.response?.status === 400) {
-    //         setErrMsg("Missing Username or Password");
-    //       } else if (err.response?.status === 401) {
-    //         setErrMsg("Unauthorized");
-    //       } else {
-    //         setErrMsg("Login Failed");
-    //       }
-    //       errRef.current.focus();
-    //     }
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response?.data));
+      //console.log(JSON.stringify(response));
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+      setAuth({ user, pwd, roles, accessToken });
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+    }
   };
 
   return (
