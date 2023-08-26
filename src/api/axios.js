d41302d3422c25ced90dea1fddef866d28336304
Miran Qarachatani <miranararat722@gmail.com>
2023-08-26 16:32:01 -0400

    add request interceptor and refresh hook

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,11 @@
 import axios from "axios";
+const BASE_URL = "http://localhost:3006";
 
 export default axios.create({
-  baseURL: "http://localhost:3006",
+  baseURL: BASE_URL,
+});
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
