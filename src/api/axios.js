d94e6e25b1347ff431acf0f10017ac7a45cbb5f6
Miran Qarachatani <miranariff1@gmail.com>
2023-10-22 22:40:37 -0400

    Some changes for axios api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 //const BASE_URL = "http://localhost:3006";
+import {baseUrl} from "shared/baseURL";
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: BaseUrl,
 });
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
